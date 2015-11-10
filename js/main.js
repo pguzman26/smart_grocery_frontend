216cb779b4592734e872942085574278a822e1a9
gmankd <pabloguzman26@yahoo.com>
2015-11-10 18:06:58 -0500

    Fixed my code for the login

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
 // Login Form
 $(function(){
-var textfield = $("input[name=user]");
-            $('button[type="submit"]').click(function(e) {
-                e.preventDefault();
-                //little validation just to check username
-                if (textfield.val() != "") {
-                    //$("body").scrollTo("#output");
-                    $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
-                    $("#output").removeClass(' alert-danger');
-                    $("input").css({
-                    "height":"0",
-                    "padding":"0",
-                    "margin":"0",
-                    "opacity":"0"
-                    });
-                    //change button text
-                    $('button[type="submit"]').html("continue")
-                    .removeClass("btn-info")
-                    .addClass("btn-default").click(function(){
-                    $("input").css({
-                    "height":"auto",
-                    "padding":"10px",
-                    "opacity":"1"
+  var textfield = $("input[name=user]");
+$('button[type="submit"]').click(function(e) {
+  e.preventDefault();
+//little validation just to check username
+  if (textfield.val() !== "") {
+//$("body").scrollTo("#output");
+  $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
+  $("#output").removeClass(' alert-danger');
+  $("input").css({
+    "height":"0",
+    "padding":"0",
+    "margin":"0",
+    "opacity":"0"
+                  });
+//change button text
+  $('button[type="submit"]').html("continue")
+      .removeClass("btn-info")
+      .addClass("btn-default").click(function(){
+  $("input").css({
+      "height":"auto",
+      "padding":"10px",
+      "opacity":"1"
                     }).val("");
                     });
 
-                    //show avatar
-                    $(".avatar").css({
-                        "background-image": "url('http://api.randomuser.me/0.3.2/portraits/women/35.jpg')"
+//show avatar
+  $(".avatar").css({
+      "background-image": "url('http://api.randomuser.me/0.3.2/portraits/women/35.jpg')"
                     });
                 } else {
-                    //remove success mesage replaced with error message
-                    $("#output").removeClass(' alert alert-success');
-                    $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username ");
+//remove success mesage replaced with error message
+  $("#output").removeClass(' alert alert-success');
+  $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username ");
                 }
                 //console.log(textfield.val());
-
             });
 });
 
@@ -69,7 +68,7 @@ $(document).on("keydown", function(e) {
 
 
 
-// Toggle delete icon when edit button is clicked
+// Toggles the delete icon when edit button is clicked
 function editButton() {
   $(".edit").on("click", "span", function() {
     $(".cross").toggle();
@@ -90,13 +89,13 @@ function getInput() {
 }
 
 
-/******************************************************************************
-  adding item to the list
-  increment id counter for unique id
-*******************************************************************************/
+
+ // adding item to the list
+ // increment id counter for unique id
+
 function addItem(message) {
 
-  $(".cross").hide(); // hiding the delete icon
+  $(".cross").hide(); // hides the delete icon
 
   var checkbox = "<td class=\"check\">" + "<input type=\"checkbox\" id=\"item" + id + "\" class=\"box\">" + "<label for=\"item" + id + "\" class=\"check-label\"></label></td>";
 

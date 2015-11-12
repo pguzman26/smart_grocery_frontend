cc48575e84167899dd2200072162dac34a7e829d
gmankd <pabloguzman26@yahoo.com>
2015-11-12 10:38:39 -0500

    Log in function is now working.

---
+++ js/api.js
@@ -0,0 +1,128 @@
+'use strict';
+var smart_grocery = {
+    url: 'http://localhost:3000',
+
+    ajax: function(config, cb) {
+        $.ajax(config).done(function(data, textStatus, jqxhr) {
+            cb(null, data);
+        }).fail(function(jqxhr, status, error) {
+            cb({
+                jqxher: jqxhr,
+                status: status,
+                error: error
+            });
+        });
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    register: function register(credentials, callback) {
+        this.ajax({
+            method: 'POST',
+            url: this.url + '/register',
+            contentType: 'application/json',
+            data: JSON.stringify(credentials),
+            dataType: 'json'
+        }, callback);
+    },
+
+    login: function login(credentials, callback) {
+        this.ajax({
+            method: 'POST',
+            url: this.url + '/login',
+            contentType: 'application/json',
+            data: JSON.stringify(credentials),
+            dataType: 'json'
+        }, callback);
+    },
+
+    logout: function logout(id, token, callback) {
+        this.ajax({
+            method: 'DELETE',
+            url: this.url + '/logout/' + user_id,
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            contentType: 'application/json',
+            dataType: 'json'
+        }, callback);
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    createProfile: function createProfile(profile, user_id, token, callback) {
+        this.ajax({
+            method: 'POST',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            contentType: 'application/json',
+            data: JSON.stringify(profile),
+            dataType: 'json'
+        }, callback);
+    },
+
+    updateProfile: function updateProfile(profile, user_id, token, callback) {
+        this.ajax({
+            method: 'PATCH',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            contentType: 'application/json',
+            data: JSON.stringify(profile),
+            dataType: 'json'
+        }, callback);
+    },
+
+    readProfile: function readProfile(user_id, token, callback) {
+        this.ajax({
+            method: 'GET',
+            url: this.url + '/users/' + user_id + '/profile',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            dataType: 'json'
+
+        }, callback);
+    },
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    //Authenticated api actions
+    listGames: function(token, callback) {
+        this.ajax({
+            method: 'GET',
+            url: this.url + '/games',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            dataType: 'json'
+        }, callback);
+    },
+
+    createGame: function(token, callback) {
+        this.ajax({
+            method: 'POST',
+            url: this.url + '/games',
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify({}),
+            dataType: 'json',
+        }, callback);
+    },
+
+    showGame: function(id, token, callback) {
+        this.ajax({
+            method: 'GET',
+            url: this.url + '/games/' + id,
+            headers: {
+                Authorization: 'Token token=' + token
+            },
+            dataType: 'json'
+        }, callback);
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,109 +1,180 @@
-// Login Form
-$(function(){
-  var textfield = $("input[name=user]");
-$('button[type="submit"]').click(function(e) {
-  e.preventDefault();
-//little validation just to check username
-  if (textfield.val() !== "") {
-//$("body").scrollTo("#output");
-  $("#output").addClass("alert alert-success animated fadeInUp").html("Welcome back " + "<span style='text-transform:uppercase'>" + textfield.val() + "</span>");
-  $("#output").removeClass(' alert-danger');
-  $("input").css({
-    "height":"0",
-    "padding":"0",
-    "margin":"0",
-    "opacity":"0"
-                  });
-//change button text
-  $('button[type="submit"]').html("continue")
-      .removeClass("btn-info")
-      .addClass("btn-default").click(function(){
-  $("input").css({
-      "height":"auto",
-      "padding":"10px",
-      "opacity":"1"
-                    }).val("");
-                    });
-
-//show avatar
-  $(".avatar").css({
-      "background-image": "url('http://api.randomuser.me/0.3.2/portraits/women/35.jpg')"
-                    });
-                } else {
-//remove success mesage replaced with error message
-  $("#output").removeClass(' alert alert-success');
-  $("#output").addClass("alert alert-danger animated fadeInUp").html("sorry enter a username ");
-                }
-                //console.log(textfield.val());
-            });
-});
-
-
-// Login Form End
+'use strict'
 
 
 var id = 1; // unique id for list items
 
+
+
 $(document).ready(function(e) {
-  editButton();
+    editButton();
+    $('#grocery').hide();
+    $('#input').hide();
+    $('#custInput').hide();
+    $('#btn').hide();
+    $('#list').hide();
+    $('#items').hide();
+    $('#edit').hide();
 
-  $("tbody").on("click", ".cross", function() {
-    $(this).closest("tr").remove();
-  });
 
-  $("button").on("click", getInput);
 
-  $("tbody").on("click", ".box", function() {
-    $(this).closest("tr").find("span").toggleClass("checked");
-  });
+    $("tbody").on("click", ".cross", function() {
+        $(this).closest("tr").remove();
+    });
+
+    $("button").on("click", getInput);
+
+    $("tbody").on("click", ".box", function() {
+        $(this).closest("tr").find("span").toggleClass("checked");
+    });
 
 });
 
 // triggered on Enter
 $(document).on("keydown", function(e) {
-  if(e.keyCode === 13) {
-    getInput();
-  }
+    if (e.keyCode === 13) {
+        getInput();
+    }
 });
 
 
 
-// Toggles the delete icon when edit button is clicked
+// Toggle delete icon when edit button is clicked
 function editButton() {
-  $(".edit").on("click", "span", function() {
-    $(".cross").toggle();
-  });
+    $(".edit").on("click", "span", function() {
+        $(".cross").toggle();
+    });
 }
 
 
 // Obtaining customer input and then calling addItem() with the input
-
 function getInput() {
-  var custInput = $(".custinput");
-  var input = custInput.val();
+    var custInput = $(".custinput");
+    var input = custInput.val();
 
-  if ((input !== "") && ($.trim(input) !== "")) {
-    addItem(input);
-    custInput.val("");
-  }
+    if ((input !== "") && ($.trim(input) !== "")) {
+        addItem(input);
+        custInput.val("");
+    }
 }
 
 
-
- // adding item to the list
- // increment id counter for unique id
-
+/******************************************************************************
+  adding item to the list
+  increment id counter for unique id
+*******************************************************************************/
 function addItem(message) {
 
-  $(".cross").hide(); // hides the delete icon
+    $(".cross").hide(); // hiding the delete icon
 
-  var checkbox = "<td class=\"check\">" + "<input type=\"checkbox\" id=\"item" + id + "\" class=\"box\">" + "<label for=\"item" + id + "\" class=\"check-label\"></label></td>";
+    var checkbox = "<td class=\"check\">" + "<input type=\"checkbox\" id=\"item" + id + "\" class=\"box\">" + "<label for=\"item" + id + "\" class=\"check-label\"></label></td>";
 
-  var content = "<td class=\"content\"><span>" + message + "</span></td>";
+    var content = "<td class=\"content\"><span>" + message + "</span></td>";
 
-  var delIcon = "<td><img src=\"img/cross.png\" alt=\"cross\" class=\"cross\"></td>";
+    var delIcon = "<td><img src=\"img/cross.png\" alt=\"cross\" class=\"cross\"></td>";
 
-  $("tbody").append("<tr>" + checkbox + content + delIcon + "</tr>");
+    $("tbody").append("<tr>" + checkbox + content + delIcon + "</tr>");
 
-  id++;
-}
+    id++;
+};
+
+
+
+
+
+////////////////Login/Register Helper
+
+
+var form2object = function(form) {
+    var data = {};
+    $(form).find('input').each(function(index, element) {
+        var type = $(this).attr('type');
+        if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+            data[$(this).attr('name')] = $(this).val();
+        }
+    });
+    return data;
+};
+
+var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+};
+
+var callback = function callback(error, data) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log(data);
+};
+
+
+
+
+
+/////////////////////////////////////////// Login Form//////////////////////////////////
+
+
+
+
+
+
+$('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+});
+$('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+
+
+});
+
+
+///////////////Register
+
+
+$('#register').on('submit', function(e) {
+        var credentials = wrap('credentials', form2object(this));
+        smart_grocery.register(credentials, function(err, data) {
+            //     if (err) { console.error(err); });
+            e.preventDefault();
+            $('#login-form').hide();
+        });
+
+    },
+
+
+
+
+    ///////////////Login
+
+
+
+    $('#login').on('submit', function(e) {
+        var credentials = wrap('credentials', form2object(this));
+        smart_grocery.login(credentials, function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                token = data.user.token;
+                var user_id = data.user.id;
+                console.log(data);
+                $('.modal-dialog').hide();
+                $('#register_form').hide();
+                $('#logout').show();
+                $("#profile_buttons_display").show();
+            }
+
+        });
+        e.preventDefault();
+
+
+    }));
